4ed16400a1a0db474b947b0bac2b657bc1f27171
IsraelMuCa <isra.munoz@gmail.com>
2018-05-11 14:02:38 -0500

    Modified HTML to better suit my needs, added pseudocode to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+/*
+
+PSEUDOCODE:
+
+- PAGE LOADS
+- GET DROPDOWN VALUE                            TBD
+- SET QUESTION AMMT TO DROPDOWN VALUE           TBD
+- START WITH BUTTON
+- START TIMER
+- SET QUESTION AND ANSWERS
+- LISTEN TO ANSWER CLICK
+- ¿CORRECT ANSWER?
+    - YES
+        - +1 CORRECT ANSWERS
+        - ¿MORE QUESTIONS?
+            - YES
+                - SET NEW QUESTION
+            - NO
+                - END GAME
+                - CALCULATE SCORE
+    - NO
+        - +1 INCORRECT ANSWERS
+        - ¿MORE QUESTIONS?
+            - YES
+                - SET NEW QUESTION
+            - NO
+                - END GAME
+                - CALCULATE SCORE
+
+*/
+
